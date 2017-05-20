1f1ac95bfdc06f53d8646101ca53404fa906160e
raventid <juliankul@gmail.com>
2017-05-20 19:44:42 +0300

    minor refactoring of tracing interface

--- lib/tracer/set_trace_func.rb
+++ lib/tracer/set_trace_func.rb
@@ -1,19 +1,30 @@
 class SetTraceFunc
   def initialize
+  end
+
+  def run
+    start_tracing
+    yield
+  rescue
+    raise
+  ensure
+    stop_tracing
+  end
+
+  def start_tracing
     start = DateTime.now.strftime('%Q').to_i / 1000.0
-    set_trace_func proc { |event, file, line, id, binding, classname|
+
+    function = proc do |event, file, line, id, binding, classname|
       now_ms = DateTime.now.strftime('%Q').to_i / 1000.0
       duration = '%.3f' % (now_ms - start)
       start = DateTime.now.strftime('%Q').to_i / 1000.0
       printf "%s %s %8s %s:%-2d %10s %8s\n", DateTime.now.strftime("%S.%L"), duration, event, file, line, id, classname
-    }
-
-    begin
-      yield
-    rescue
-      raise
-    ensure
-      set_trace_func(nil)
     end
+
+    set_trace_func(function) 
+  end
+
+  def stop_tracing
+    set_trace_func(nil)
   end
 end
--- spec/set_trace_func_spec.rb
+++ spec/set_trace_func_spec.rb
@@ -4,13 +4,13 @@ require_relative "../lib/tracer/set_trace_func"
 describe SetTraceFunc do
   context "trace function do not raise exception" do
     it "should show me some trace" do
-      SetTraceFunc.new { puts "hello" }
+      SetTraceFunc.new.run { puts "hello" }
     end
   end
 
   context "stop tracing and reraise exception if any" do
     it "stop tracing" do
-      expect { SetTraceFunc.new {raise "Catch me"} }.to raise_error StandardError
+      expect { SetTraceFunc.new.run { raise "Catch me" } }.to raise_error StandardError
     end
   end
 end
