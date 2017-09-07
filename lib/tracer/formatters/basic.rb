28582897fc68d5641ad5c1e156da1e7baa8a7c90
Julian Kulesh <julian.kulesh@kupibilet.ru>
2017-09-08 00:17:44 +0300

    and final move - read DateTime from module root

--- lib/tracer/formatters/basic.rb
+++ lib/tracer/formatters/basic.rb
@@ -4,13 +4,13 @@ module Tracer
   module Formatters
     class Basic
       def self.call
-        start = DateTime.now.strftime('%Q').to_i / 1000.0
+        start = ::DateTime.now.strftime('%Q').to_i / 1000.0
 
         proc do |event, file, line, id, binding, classname|
-          now_ms = DateTime.now.strftime('%Q').to_i / 1000.0
+          now_ms = ::DateTime.now.strftime('%Q').to_i / 1000.0
           duration = '%.3f' % (now_ms - start)
-          start = DateTime.now.strftime('%Q').to_i / 1000.0
-          printf "%s %s %8s %s:%-2d %10s %8s\n", DateTime.now.strftime("%S.%L"), duration, event, file, line, id, classname
+          start = ::DateTime.now.strftime('%Q').to_i / 1000.0
+          printf "%s %s %8s %s:%-2d %10s %8s\n", ::DateTime.now.strftime("%S.%L"), duration, event, file, line, id, classname
         end
       end
     end
